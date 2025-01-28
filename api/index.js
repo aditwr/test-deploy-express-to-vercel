61412235a1e688167d15bd299881f2d060abdbf1
aditya <awahyu148@gmail.com>
2025-01-28 13:17:35 +0700

    update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/api", (req, res) =>
+  res.json({
+    message: "Hello from the API",
+  })
+);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
